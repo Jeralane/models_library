df3ec48c7b389987dfffb373b6a5572f404ec4c0
Jess Alejo <jess.alejo@innohub.ph>
2014-03-28 16:11:54 +0800

    Create a class to organize the querying of all items for certain type of model

---
+++ ModelCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ModelsLibrary
+{
+    public static class ModelCollection
+    {
+        public static ObservableCollection<T> All<T>() where T : AbstractModel, new()
+        {
+            var item = new T();
+
+            var sql = "SELECT * FROM " + item.TableName();
+            var collection = new ObservableCollection<T>();
+            var dataTable = DatabaseController.ExecuteSelectQuery(sql);
+            foreach (System.Data.DataRow dataRow in dataTable.Rows)
+            {
+                var model = new T();
+                model.SetPropertiesFromDataRow(dataRow);
+                collection.Add(model);
+            }
+            return collection;
+        }
+    }
+}
--- Payroll/Billing.cs
+++ Payroll/Billing.cs
@@ -1,109 +1,94 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 
 namespace ModelsLibrary.Payroll
 {
-    public class Billing : AbstractModel
+    public partial class Billing : AbstractModel
     {
         private string _billingType;
-        private int _billingNumber;
-        private int _detachmentId;
-        private int _payrollPeriodId;
-        private decimal _monthlyContractAmount;
-        private decimal _agencyFee;
-        private decimal _agencyFeeVat;
-        private decimal _agencyFeeTotal;
-        private decimal _ewt;
-        private decimal _netAgencyFee;
-        private decimal _totalEmployeeSalary;
-        private decimal _totalAmountCollectable;
-
         public string BillingType
         {
             get { return _billingType; }
             set { _billingType = value; OnPropertyChanged("BillingType"); }
         }
 
+        private int _billingNumber;
         public int BillingNumber
         {
             get { return _billingNumber; }
             set { _billingNumber = value; OnPropertyChanged("BillingNumber"); }
         }
 
+        private int _detachmentId;
         public int DetachmentId
         {
             get { return _detachmentId; }
             set { _detachmentId = value; OnPropertyChanged("DetachmentId"); }
         }
 
+        private int _payrollPeriodId;
         public int PayrollPeriodId
         {
             get { return _payrollPeriodId; }
             set { _payrollPeriodId = value; OnPropertyChanged("PayrollPeriodId"); }
         }
 
+        private decimal _monthlyContractAmount;
         public decimal MonthlyContractAmount
         {
             get { return _monthlyContractAmount; }
             set { _monthlyContractAmount = value; OnPropertyChanged("MonthlyContractAmount"); }
         }
 
+        private decimal _agencyFee;
         public decimal AgencyFee
         {
             get { return _agencyFee; }
             set { _agencyFee = value; OnPropertyChanged("AgencyFee"); }
         }
 
-        public decimal AgencyFeeVat
+        private decimal _agencyVatFee;
+        public decimal AgencyVatFee
         {
-            get { return _agencyFeeVat; }
-            set { _agencyFeeVat = value; OnPropertyChanged("AgencyFeeVat"); }
+            get { return _agencyVatFee; }
+            set { _agencyVatFee = value; OnPropertyChanged("AgencyVatFee"); }
         }
 
+        private decimal _agencyFeeTotal;
         public decimal AgencyFeeTotal
         {
             get { return _agencyFeeTotal; }
             set { _agencyFeeTotal = value; OnPropertyChanged("AgencyFeeTotal"); }
         }
 
-        public decimal EWT
+        private decimal _ewt;
+        public decimal Ewt
         {
             get { return _ewt; }
-            set { _ewt = value; OnPropertyChanged("EWT"); }
+            set { _ewt = value; OnPropertyChanged("Ewt"); }
         }
 
+        private decimal _netAgencyFee;
         public decimal NetAgencyFee
         {
             get { return _netAgencyFee; }
             set { _netAgencyFee = value; OnPropertyChanged("NetAgencyFee"); }
         }
 
+        private decimal _totalEmployeeSalary;
         public decimal TotalEmployeeSalary
         {
             get { return _totalEmployeeSalary; }
             set { _totalEmployeeSalary = value; OnPropertyChanged("TotalEmployeeSalary"); }
         }
 
+        private decimal _totalAmountCollectable;
         public decimal TotalAmountCollectable
         {
             get { return _totalAmountCollectable; }
             set { _totalAmountCollectable = value; OnPropertyChanged("TotalAmountCollectable"); }
         }
 
-
-        public static LinkedList<Billing> GetAll()
-        {
-            var collection = new LinkedList<Billing>();
-            const string sql = "SELECT * FROM Billings";
-            var dataTable = DatabaseController.ExecuteSelectQuery(sql);
-            foreach (System.Data.DataRow dataRow in dataTable.Rows)
-            {
-                var item = new Billing();
-                item.SetPropertiesFromDataRow(dataRow);
-                collection.AddLast(item);
-            }
-
-            return collection;
-        }
     }
+
 }
